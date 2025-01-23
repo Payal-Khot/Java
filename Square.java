6b00e888a7276ce1145a14669377e2640de15410
Payal-Khot <159454828+Payal-Khot@users.noreply.github.com>
2025-01-23 21:26:59 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,11 @@
+ public class Square{
+    public static void main(String[] args)
+    {
+        int side = 10;
+        double area = side*side;
+        System.out.println("Area " + area);
+        double perimeter = 4*side;
+        System.out.println("Perimeter " + perimeter);
+
+    }
+}
